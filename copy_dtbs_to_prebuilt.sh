4f3378f3fc66d3401b269e2579cd95633eedf1ea
alexklimaj <alex@arkelectron.com>
2024-05-31 15:58:17 -0600

    Copy dtbs to ark compiled git folder

--- copy_dtbs_to_prebuilt.sh
+++ copy_dtbs_to_prebuilt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DTBS_SOURCE_PATH="$ARK_JETSON_KERNEL_DIR/source_build/Linux_for_Tegra/source/nvidia-oot/device-tree/platform/generic-dts/dtbs"
 PREBUILT_PATH="$ARK_JETSON_KERNEL_DIR/prebuilt/Linux_for_Tegra"
+ARK_COMPILED_DEVICE_TREE_PATH="$ARK_JETSON_KERNEL_DIR/prebuilt/ark_jetson_compiled_device_tree_files/Linux_for_Tegra"
 
 echo "Installing DTBs into prebuilt directory"
 # Copy kernel device tree to bootloader path
@@ -55,3 +56,26 @@ do
         sudo rm $filepath
     fi
 done
+
+echo "Installing DTBs into ark_jetson_compiled_device_tree_files directory"
+# Copy kernel device tree to bootloader path
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3768-0000+p3767-0000-nv.dtb $ARK_COMPILED_DEVICE_TREE_PATH/rootfs/boot/
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3768-0000+p3767-0001-nv.dtb $ARK_COMPILED_DEVICE_TREE_PATH/rootfs/boot/
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3768-0000+p3767-0003-nv.dtb $ARK_COMPILED_DEVICE_TREE_PATH/rootfs/boot/
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3768-0000+p3767-0004-nv.dtb $ARK_COMPILED_DEVICE_TREE_PATH/rootfs/boot/
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3768-0000+p3767-0000-dynamic.dtbo $ARK_COMPILED_DEVICE_TREE_PATH/rootfs/boot/
+
+# Copy kernel device tree to kernel path
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3768-0000+p3767-0000-nv.dtb $ARK_COMPILED_DEVICE_TREE_PATH/kernel/dtb/
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3768-0000+p3767-0001-nv.dtb $ARK_COMPILED_DEVICE_TREE_PATH/kernel/dtb/
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3768-0000+p3767-0003-nv.dtb $ARK_COMPILED_DEVICE_TREE_PATH/kernel/dtb/
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3768-0000+p3767-0004-nv.dtb $ARK_COMPILED_DEVICE_TREE_PATH/kernel/dtb/
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3768-0000+p3767-0000-dynamic.dtbo $ARK_COMPILED_DEVICE_TREE_PATH/kernel/dtb/
+
+# Copy camera overlays to kernel and bootloader paths
+# IMX477 Single
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3767-camera-p3768-ark-imx477-single.dtbo $ARK_COMPILED_DEVICE_TREE_PATH/rootfs/boot/
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3767-camera-p3768-ark-imx477-single.dtbo $ARK_COMPILED_DEVICE_TREE_PATH/kernel/dtb/
+# IMX219 Quad
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3767-camera-p3768-ark-imx219-quad.dtbo $ARK_COMPILED_DEVICE_TREE_PATH/rootfs/boot/
+sudo cp $DTBS_SOURCE_PATH/tegra234-p3767-camera-p3768-ark-imx219-quad.dtbo $ARK_COMPILED_DEVICE_TREE_PATH/kernel/dtb/
