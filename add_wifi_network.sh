d4629b90dce040e288c56e673505d36be0dbe240
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-10-14 17:54:26 -0800

    cleanup

--- add_wifi_network.sh
+++ add_wifi_network.sh
@@ -13,14 +13,11 @@ TEMPLATE="ConnectionTemplate.nmconnection"
 DEST_DIR="prebuilt/Linux_for_Tegra/rootfs/etc/NetworkManager/system-connections"
 OUTPUT_FILE="${DEST_DIR}/${SSID}.nmconnection"
 
-# Ensure the destination directory exists
 sudo mkdir -p "$DEST_DIR"
 sudo cp $TEMPLATE $OUTPUT_FILE
 
-# TODO: replace all occurences of YourNetworkSSID with $SSID and YourNetworkPassword with $PASSWORD
 sudo sed -i "s/YourNetworkSSID/$SSID/g; s/YourNetworkPassword/$PASSWORD/g" "$OUTPUT_FILE"
 
-# Set the correct ownership and permissions for the new network connection file
 sudo chown root:root "$OUTPUT_FILE"
 sudo chmod 600 "$OUTPUT_FILE"
 
