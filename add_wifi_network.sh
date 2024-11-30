1f16d8707a30732356679c7921868204a80082b0
Jacob Dahl <37091262+dakejahl@users.noreply.github.com>
2024-11-30 11:45:53 -0900

    fix script syntax (#16)

--- add_wifi_network.sh
+++ add_wifi_network.sh
@@ -13,12 +13,12 @@ TEMPLATE="ConnectionTemplate.nmconnection"
 DEST_DIR="prebuilt/Linux_for_Tegra/rootfs/etc/NetworkManager/system-connections"
 OUTPUT_FILE="${DEST_DIR}/${SSID}.nmconnection"
 
-sudo mkdir -p "$DEST_DIR"
-sudo cp $TEMPLATE $OUTPUT_FILE
+sudo mkdir -p "${DEST_DIR}"
+sudo cp $TEMPLATE "${OUTPUT_FILE}"
 
-sudo sed -i "s/YourNetworkSSID/$SSID/g; s/YourNetworkPassword/$PASSWORD/g" "$OUTPUT_FILE"
+sudo sed -i "s/YourNetworkSSID/${SSID}/g; s/YourNetworkPassword/${PASSWORD}/g" "${OUTPUT_FILE}"
 
-sudo chown root:root "$OUTPUT_FILE"
-sudo chmod 600 "$OUTPUT_FILE"
+sudo chown root:root "${OUTPUT_FILE}"
+sudo chmod 600 "${OUTPUT_FILE}"
 
-echo "Network configuration for SSID '$SSID' has been created at $OUTPUT_FILE."
+echo "Network configuration for SSID '${SSID}' has been created at ${OUTPUT_FILE}."
