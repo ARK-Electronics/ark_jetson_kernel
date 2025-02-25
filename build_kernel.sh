c66db2ef934cc6e3877a1584e9c2f91f771f8b13
Jacob Dahl <37091262+dakejahl@users.noreply.github.com>
2025-02-24 16:37:31 -0900

    Add imx219 tuning params to filesystem (#22)
    * add imx219 camera params to prebuilt filesystem
    
    * clean up tar file

--- build_kernel.sh
+++ build_kernel.sh
@@ -19,6 +19,7 @@ make -C kernel && make modules && make dtbs
 sudo -E make install -C kernel
 cp kernel/kernel-jammy-src/arch/arm64/boot/Image ../../../prebuilt/Linux_for_Tegra/kernel/
 $ARK_JETSON_KERNEL_DIR/copy_dtbs_to_prebuilt.sh
+$ARK_JETSON_KERNEL_DIR/copy_camera_params_to_prebuilt.sh
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$((${END_TIME}-${START_TIME}))
---
+++ copy_camera_params_to_prebuilt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DOWNLOAD_URL="https://files.waveshare.com/upload/e/eb/Camera_overrides.tar.gz"
+TAR_FILE="Camera_overrides.tar.gz"
+ISP_FILE="camera_overrides.isp"
+DEST_DIR="$ARK_JETSON_KERNEL_DIR/prebuilt/Linux_for_Tegra/rootfs/var/nvidia/nvcam/settings"
+
+if [ -f "$DEST_DIR/$ISP_FILE" ]; then
+    echo "Camera overrides file already exists at $DEST_DIR/$ISP_FILE"
+    exit 0
+fi
+
+echo "Camera overrides file not found in $DEST_DIR. Downloading and installing..."
+
+echo "Downloading $TAR_FILE..."
+wget "$DOWNLOAD_URL"
+
+echo "Extracting $TAR_FILE..."
+tar zxvf "$TAR_FILE"
+
+echo "Copying $ISP_FILE to $DEST_DIR..."
+sudo mv "$ISP_FILE" "$DEST_DIR/"
+
+echo "Installation completed successfully!"
+
+rm $TAR_FILE
